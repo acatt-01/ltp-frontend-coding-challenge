cbfcf1b6dc4c45fec1acaa7a6240dd1a4ea8fae7
acatt01 <1230166@isep.ipp.pt>
2025-10-05 22:54:04 +0100

    added netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@remix-run/netlify";
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths(), netlifyPlugin()],
 });
